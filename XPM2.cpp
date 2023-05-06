c47ec08f70425884769b1001a36517d7d5176cf0
Cláudia Mesquita <cmmesquita3104@gmail.com>
2023-05-06 21:37:29 +0100

    WIP read XPM2

--- XPM2.cpp
+++ XPM2.cpp
@@ -1,8 +1,44 @@
 #include "XPM2.hpp"
+#include <sstream>
+#include <vector>
+#include <map>
+#include <iostream>
 
 namespace prog {
-    Image* loadFromXPM2(const std::string& file) {
-        return nullptr;
+    Image* loadFromXPM2(const std::string& file){
+        int width, height, numberOfColors, charactersPerPixel, hexValue;
+        char colorReplacement, type;
+        rgb_value red;
+        rgb_value green;
+        rgb_value blue;
+        std::map<char, Color> colorKey;
+        std::stringstream ss(file);
+
+        ss.ignore(256, '/n');
+
+        ss >> width;
+        ss >> height;
+        ss >> numberOfColors;
+        ss >> charactersPerPixel;
+
+        std::vector<std::vector<Color>> convertedImage(width,std::vector<Color>(height));
+
+        for (int i = 0; i <= numberOfColors; i++) {
+            ss >> colorReplacement;
+            ss >> type;
+            ss >> hexValue;
+            red = ((hexValue >> 16) & 0xFF) / 255.0;
+            green = ((hexValue >> 8) & 0xFF) / 255.0;
+            blue = ((hexValue) & 0xFF) / 255.0; 
+            colorKey[colorReplacement] = Color(red,green,blue);
+        }
+
+        std::string row;
+        while (ss >> row){
+            std::cout << row;
+        }
+        
+
     }
 
     void saveToXPM2(const std::string& file, const Image* image) {
